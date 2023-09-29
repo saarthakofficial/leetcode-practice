579c77c4a22108b489f414c747b9675ddbb2dd13
Saarthak Srivastava <saarthak2k@gmail.com>
2023-09-29 16:18:04 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (15.12%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -7,19 +7,14 @@ public:
 
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
+                nums1[k--] = nums1[i--];
             } else {
-                nums1[k] = nums2[j];
-                j--;
+                nums1[k--] = nums2[j--];
             }
-            k--;
         }
         
         while (j>=0){
-            nums1[k] = nums2[j];
-            j--;
-            k--;
+            nums1[k--] = nums2[j--];
         }
     }
 };
