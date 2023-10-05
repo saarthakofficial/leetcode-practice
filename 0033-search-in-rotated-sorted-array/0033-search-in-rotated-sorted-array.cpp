6ac868582a53001d514dc071268efc170320bf91
Saarthak Srivastava <saarthak2k@gmail.com>
2023-10-06 00:10:20 +0530

    Time: 3 ms (64.94%), Space: 11.3 MB (11.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,11 +1,56 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for (int i=0; i<nums.size(); i++){
-            if (nums[i]==target){
-                return i;
+    int getPivot(vector<int>& arr, int n) {
+
+        int s = 0;
+        int e = n-1;
+        int mid = s + (e-s)/2;
+
+        while(s<e) {
+
+            if(arr[mid] >= arr[0])
+            {
+                s = mid+1;
             }
+            else{
+                e = mid;
+            }
+            mid = s + (e-s)/2;
         }
+        return s;
+    }
+    int binarySearch(vector<int>& arr, int s, int e, int key) {
+
+        int start = s;
+        int end = e;
+
+        int mid = start + (end-start)/2;
+
+        while(start <= end) {
+
+            if(arr[mid] == key) {
+                return mid;
+            }
+
+            if(key > arr[mid]) {
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+
+            mid = start + (end-start)/2;
+        }
+
         return -1;
     }
+    int search(vector<int>& nums, int target) {
+        int pivot = getPivot(nums, nums.size());
+        if( target >= nums[pivot] && target <= nums[nums.size()-1]){
+            return binarySearch(nums, pivot, nums.size()-1, target);
+        }
+        else{
+            return binarySearch(nums, 0, pivot - 1, target);
+        }
+  }
 };
