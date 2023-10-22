455ce8c70c20bf5e535fafd348819aaa1e3f5e3d
Saarthak Srivastava <saarthak2k@gmail.com>
2023-10-22 19:11:13 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (24.95%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -6,43 +6,48 @@ public:
         int sign = 1;
         int index = 0;
 
-        while (index < s.size()) {
+        while (index < s.size()){
             char c = s[index];
             
-            if (c == ' ' && !start) {
+            if (c == ' ' && !start){
                 index++;
                 continue;
             }
             
-            if (!start) {
-                if (c == '+') {
+            if (!start){
+                if (c == '+'){
                     start = true;
                     index++;
-                } else if (c == '-') {
+                }
+                else if (c == '-'){
                     sign = -1;
                     start = true;
                     index++;
-                } else if (c >= '0' && c <= '9') {
+                }
+                else if (c >= '0' && c <= '9'){
                     start = true;
                     i = c - '0';
                     index++;
-                } else {
+                }
+                else{
                     break;
                 }
-            } else {
-                if (c >= '0' && c <= '9') {
+            }
+            else{
+                if (c >= '0' && c <= '9'){
                     int digit = c - '0';
                     i = i * 10 + digit;
                     
-                    if (sign == 1 && i > INT_MAX) {
+                    if (sign == 1 && i > INT_MAX){
                         return INT_MAX;
                     }
-                    if (sign == -1 && -i < INT_MIN) {
+                    if (sign == -1 && -i < INT_MIN){
                         return INT_MIN;
                     }
                     
                     index++;
-                } else {
+                }
+                else{
                     break;
                 }
             }
