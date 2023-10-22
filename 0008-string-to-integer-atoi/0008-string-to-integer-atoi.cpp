1a0eb84264ad098bc29d8ec5877cf160983b221d
Saarthak Srivastava <saarthak2k@gmail.com>
2023-10-22 19:09:27 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (60.37%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        long long i = 0;
+        bool start = false;
+        int sign = 1;
+        int index = 0;
+
+        while (index < s.size()) {
+            char c = s[index];
+            
+            if (c == ' ' && !start) {
+                index++;
+                continue;
+            }
+            
+            if (!start) {
+                if (c == '+') {
+                    start = true;
+                    index++;
+                } else if (c == '-') {
+                    sign = -1;
+                    start = true;
+                    index++;
+                } else if (c >= '0' && c <= '9') {
+                    start = true;
+                    i = c - '0';
+                    index++;
+                } else {
+                    break;
+                }
+            } else {
+                if (c >= '0' && c <= '9') {
+                    int digit = c - '0';
+                    i = i * 10 + digit;
+                    
+                    // Check for overflow
+                    if (sign == 1 && i > INT_MAX) {
+                        return INT_MAX;
+                    }
+                    if (sign == -1 && -i < INT_MIN) {
+                        return INT_MIN;
+                    }
+                    
+                    index++;
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        return sign * i;
+    }
+};
